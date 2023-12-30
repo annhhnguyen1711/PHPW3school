8eb28501d4343f5b55341d226810a7683843c1e4
annhhnguyen1711 <143236128+annhhnguyen1711@users.noreply.github.com>
2023-12-30 08:55:15 +0700

    opp

---
+++ Classes_Objects_2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+    class Fruit
+    {
+        public $name;
+        function set_name($name)
+        {
+            $this->name =$name ;
+        }
+    }
+?>
+</body>
+</html>
